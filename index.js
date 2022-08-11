1cb848a684b5fbb318bcecc891138942810030cb
fluffyshark <robin.yjord1@gmail.com>
2022-08-11 18:26:56 +0200

    cors update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://astounding-cobbler-6f7739.netlify.app/",
         methods: ["POST", "GET"]
     }
 })
