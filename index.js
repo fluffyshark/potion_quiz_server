a2e9a6c902b976fa4d52716bec53445768300eac
fluffyshark <robin.yjord1@gmail.com>
2022-08-11 17:51:55 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const app = express()
+const http = require("http")
+const {Server} = require("socket.io")
+const cors = require("cors")
+
+app.use(cors())
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["POST", "GET"]
+    }
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+io.on("connection", (socket) => {
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+      });
+
+      socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+    });
+});
+
+server.listen(3001, () => {
+    console.log("server is running")
+})
