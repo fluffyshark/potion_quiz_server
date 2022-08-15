e1d0f44cfeb87be32d5de202c743ab53621875d0
fluffyshark <robin.yjord1@gmail.com>
2022-08-15 18:37:03 +0200

    socket update

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "https://astounding-cobbler-6f7739.netlify.app/",
+     //   origin: "https://astounding-cobbler-6f7739.netlify.app/",
+        origin: "http://localhost:3000",
         methods: ["POST", "GET"]
     }
 })
@@ -18,14 +19,17 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
 
+  
 io.on("connection", (socket) => {
 
     socket.on("join_room", (data) => {
         socket.join(data);
+        console.log("Join", data)
       });
 
       socket.on("send_message", (data) => {
-        socket.to(data.room).emit("receive_message", data);
+        socket.to(data.gameCode).emit("receive_message", data);
+        console.log("receive_message", data)
     });
 });
 
