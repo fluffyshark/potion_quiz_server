8d15cbf695c84e7beb774afd01ea1c43b5e3d83b
fluffyshark <robin.yjord1@gmail.com>
2022-09-04 08:51:06 +0200

    Players receive all player info when host starts game

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "https://astounding-cobbler-6f7739.netlify.app/",
-    //    origin: "http://localhost:3000",
+    //    origin: "https://astounding-cobbler-6f7739.netlify.app/",
+        origin: "http://localhost:3000",
         methods: ["POST", "GET"]
     }
 })
@@ -19,20 +19,45 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
 
+  let gameData = []
+  let gameCode;
   
 io.on("connection", (socket) => {
 
-    socket.on("join_room", (data) => {
-        socket.join(data);
-        console.log("Join", data)
-      });
+  // Player and host join the same room
+  socket.on("join_room", (data) => {socket.join(data); gameCode = data});
 
-      socket.on("send_message", (data) => {
-        socket.to(data.gameCode).emit("receive_message", data);
-        console.log("receive_message", data)
-    });
+  // When player join, then player data is sent to host, player nickname are displayed on host screen
+  socket.on("player_joining", (data) => {
+    socket.to(data.gameCode).emit("player_accepted", data);
+    gameData.push({id: socket.id, playerName: data.nickname, cards: data.cards, gameCode: data.gameCode})
+  });
+
+  // When host press "Start Game" all players are directed to QuizView
+  socket.on("ready_game", (data) => {io.in(data).emit("start_game", gameData); console.log(gameData)});
+
+  // playerData returns {player: string, points: int}
+  socket.on("sending_player_cards", (playerData) => {
+    // Updating all players' collected game data every time a player get a new card
+    updateGameData(playerData)
+    // Sending all players' collected game data to all players in the same room
+    io.in(gameCode).emit("sending_server_gameData", gameData);
+  });
+
+
+
 });
+
+
+// Updating all players' collected game data every time a player get a new card
+function updateGameData(playerData) {
+  gameData.map((player) => {if (player.playerName === playerData.playerName) {player.cards = playerData.cards, player.coins = playerData.coins}})
+}
+
+
+
 
 server.listen(3001, () => {
     console.log("server is running")
 })
+
