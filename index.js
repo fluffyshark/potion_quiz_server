1a7a9e2ac8cfb4292d01c716b7218b0b19705922
fluffyshark <robin.yjord1@gmail.com>
2022-09-24 12:16:32 +0200

    Deployment - Testing server deployment-2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {cors: {origin: '*'}});
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 /*
 const express = require("express")
